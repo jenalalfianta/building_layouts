6e102351fe14ec73d8a38ec565cfff2f2b02b33e
jenalalfianta <jenalalfianta@gmail.com>
2022-01-27 16:19:14 +0700

    penambahan gambar, tutorial selesai

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,18 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE')
+      ],
+    );
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -21,7 +33,15 @@ class _MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            titleSection
+            Image.asset(
+              'images/lake.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover
+            ),
+            titleSection,
+            buttonSection,
+            textSection
           ],
         ),
       ),
@@ -71,3 +91,16 @@ Column _buildButtonColumn(Color color, IconData icon, String label){
     ],
   );
 }
+
+Widget textSection = Padding(
+  padding: EdgeInsets.all(32),
+  child:Text(    
+    'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+    'Alps. Situated 1,578 meters above sea level, it is one of the '
+    'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
+    'half-hour walk through pastures and pine forest, leads you to the '
+    'lake, which warms to 20 degrees Celsius in the summer. Activities '
+    'enjoyed here include rowing, and riding the summer toboggan run.',
+    softWrap: true,
+  ),
+);
